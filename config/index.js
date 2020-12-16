f06442da97011cbfd2912432f786e9210f070db3
wdd <296151852@qq.com>
2020-12-16 11:20:33 +0800

    dist

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: './', // 打包加点
+    assetsPublicPath: '/', // 打包加点
     proxyTable: {},
 
     // Various Dev Server settings
